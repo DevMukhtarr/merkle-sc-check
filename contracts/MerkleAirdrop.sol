acb67e8be364de002c61508ed66634fe1bbd3dca
Mukhtar Abdulfatai <mukhtarabdulfatai@gmail.com>
2024-09-03 15:18:29 +0100

    added token address to contructor

--- contracts/MerkleAirdrop.sol
+++ contracts/MerkleAirdrop.sol
@@ -1,18 +1,27 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.24;
 
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
 contract MerkleAirdrop {
     address public owner;
     bytes32 public  merkleRoot;
+    IERC20 public tokenAddress;
     bool claimed;
 
-    constructor(address _erc20_address, bytes32  _merkleRoot) {
-         owner = msg.sender;
-         merkleRoot = merkleRoot;
+    constructor(address _tokenAddress, bytes32 _merkleRoot) {
+        owner = msg.sender;
+        merkleRoot = merkleRoot;
+        tokenAddress = IERC20(_tokenAddress);
     }   
 
     modifier onlyOwner () {
         require(msg.sender == owner, "Only owner can perform this action");
         _;
+    }
+
+    function claimReward() public{
+
     }
 }
