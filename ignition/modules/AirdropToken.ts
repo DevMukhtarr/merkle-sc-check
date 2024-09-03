649c62104012727cdbf58ff8a128d7a71be57f19
Mukhtar Abdulfatai <mukhtarabdulfatai@gmail.com>
2024-09-03 15:35:14 +0100

    modified module code for creating token

--- ignition/modules/AirdropToken.ts
+++ ignition/modules/AirdropToken.ts
@@ -1,17 +1,10 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI: bigint = 1_000_000_000n;
+const AirdropTokenModule = buildModule("LockModule", (m) => {
 
-const LockModule = buildModule("LockModule", (m) => {
-  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
-  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
+  const airdrop_token = m.contract("AirdropToken");
 
-  const lock = m.contract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  return { lock };
+  return { airdrop_token };
 });
 
-export default LockModule;
+export default AirdropTokenModule;
